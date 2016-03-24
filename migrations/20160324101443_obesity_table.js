90fa8e2f036e5f72db6e00be487ec4947b7185c2
Lissa Walzer <lissawalzer@gmail.com>
2016-03-24 10:40:11 -0700

    Added db migration

---
+++ migrations/20160324101443_obesity_table.js
@@ -0,0 +1,25 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('obesity_data', function(table) {
+        table.increments();
+        table.string('location');
+        table.string('location_name');
+        table.integer('year');
+        table.integer('age_group_id');
+        table.string('age_group');
+        table.integer('age_start');
+        table.integer('age_end');
+        table.integer('sex_id');
+        table.string('sex');
+        table.string('unit');
+        table.string('metric');
+        table.string('measure');
+        table.float('mean');
+        table.float('lower');
+        table.float('upper');
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTable('obesitydata');
+};
